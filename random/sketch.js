ae2be3f34db66cf791b3e05c18c6e051765275df
wani5272 <205021230+wani5272@users.noreply.github.com>
2025-03-31 10:14:42 +0200

    add random

---
+++ random/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  strokeWeight(2);
+}
+
+function draw() {
+  background (0, 0, 0, 10);
+  for(let i = 0 ; i<10 ; i++) {
+    stroke(random(255), random(255), random(255));
+    let x = random(0, width);
+    let y = random(0, height);
+   // line(width / 2, height / 2, x, y);
+    line(width/2 , y, x, sin(y));
+  }
+}
