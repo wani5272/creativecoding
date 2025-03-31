6f25807d5e424db78258d17cf8c117f871ca86fd
wani5272 <205021230+wani5272@users.noreply.github.com>
2025-03-31 10:23:48 +0200

    add interactivity

---
+++ sketch.js
@@ -0,0 +1,18 @@
+let r, g, b = 0
+function setup() {
+  createCanvas(400, 400);
+  strokeWeight(10);
+}
+
+function draw() {
+  background(0);
+  stroke(r, b, b);
+  fill(r, g, b, 128);
+  ellipse(width/2, height/2, mouseX, mouseY);
+}
+
+function mousePressed() {
+          r = random(255);
+          g = random(255);
+          b = random(255);
+}
