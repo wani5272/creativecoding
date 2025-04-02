ce3e0114e99aec10ecf39189830db1a83664076d
wani5272 <205021230+wani5272@users.noreply.github.com>
2025-04-02 10:21:34 +0200

    a

--- coloredrectangles/sketch.js
+++ coloredrectangles/sketch.js
@@ -1,5 +1,15 @@
 function setup() {
-
-  createCanvas(800, 800);
-  
+  createCanvas(900, 900);
+  let size = 50;  
+  let xcount = 4;
+  let ycount = 4;
+  for (let i = 0; i < ycount; i++ ) {
+    y = i*55+5;
+    for (let j = 0; j < xcount; j++) {
+      x = j*55+5;
+      xminoffset = x + random(size);
+      yminoffset = y + random(size);
+      square(x, y, size);
+    }
+  }
 }
