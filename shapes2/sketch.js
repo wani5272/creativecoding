2ebda8ae5285a6ad9c596060dd91073ab7c9152f
wani5272 <205021230+wani5272@users.noreply.github.com>
2025-04-09 09:45:18 +0200

    a

--- shapes2/sketch.js
+++ shapes2/sketch.js
@@ -1,14 +1,41 @@
+const amountOfFormPoints = 5;
+const stepSize = 2;
+const initRadius = 150;
+const mouseAttraction = 0.01;
+let centerX, centerY;
+let x = [];
+let y = [];
+
 function setup() {
-  createCanvas(400, 400);
-  background(200);
-  stroke("black");
-  strokeWeight(10);
+  createCanvas(windowWidth, windowHeight);
+  centerX = width / 2;
+  centerY = height / 2;
+  const angle = radians(360 / amountOfFormPoints);
+  for (let i = 0; i < amountOfFormPoints; i++) {
+    x.push(cos(angle * i) * initRadius);
+    y.push(cos(angle * i) * initRadius);
+  }
 
-  point(50, 50);
-  line(0, 350, 400, 300);
-  strokeWeight(2);
+  stroke(0, 75);
+  strokeWeight(0.5);
+  background(255);
+  noFill();
+}
+function draw() {
+  centerX += (mouseX - centerX) * mouseAttraction;
+  centerY += (mouseY - centerY) * mouseAttraction;
+  for (let i = 0; i < amountOfFormPoints; i++) {
+    x[i] += random(-stepSize, stepSize);
+    y[i] += random(-stepSize, stepSize);
+    ellipse(x[i] + centerX, y[i], centerY, 5, 5);
+  }
 
-  triangle(100, 100, 150, 100, 100, 250);
-  square(200, 200, 50);
-  rect(300, 100, 75, 75, 20);
+  beginShape();
+
+  curveVertex(x[0] + centerX, y[0] + centerY);
+  for (let i = 0; i < amountOfFormPoints; i++) {
+    curveVertex(x[i] + centerX, y[i] + centerY);
+  }
+
+  curveVertex(x);
 }
